ae257c24fdd7d2164517aceaf682db1277439291
hzbonny <40246758+hzbonny@users.noreply.github.com>
2018-06-14 15:57:01 +0200

    Add files via upload

---
+++ es.php
@@ -0,0 +1,24 @@
+<?php
+	$armate=[
+		'Armata Rossa',
+		'Brithish Army',
+		'Forze armate della republica sociale italiana',
+		'Forze speciali italiane',
+		'Milizia Volontaria per la Sicurezza Nazionale‎',
+		'Regia Aeronautica‎',
+		'Regia Marina‎',
+		'Regio Esercito',
+		'Forze armate del Regno del Sud‎',
+		'Royal Air Force',
+		'Royal Navy',
+		'Waffen-SS',
+		'Wehrmacht‎',
+		'II Corps (United States Army)',
+		' Luftwaffe ',
+		'Marina imperiale giapponese',
+		'United States Army Air Forces',
+		'Secondo corpo polacco',
+		'588º Reggimento bombardamento notturno',
+		'Panzer-Division',
+	];
+?>
